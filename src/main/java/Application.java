c9669cd4d8e30f606b204a7ef641a03e58fe5665
jiaweizhang <jiaweizhang95@gmail.com>
2016-04-09 22:51:13 -0400

    Running on port 80 now

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import static spark.Spark.port;
 import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
@@ -23,6 +24,7 @@ import static spark.Spark.staticFileLocation;
  */
 public class Application {
     public static void main(String args[]) {
+        port(80);
         staticFileLocation("/public");
 
         post("/api/assemble", (req, res) -> {
