b744e46b86e1b9ffde970eb50c0b543fab7343eb
jiaweizhang <jiaweizhang95@gmail.com>
2016-04-17 22:36:33 -0400

    Using ace editor now and everything looks pretty

--- webapp/src/components/Assembler.vue
+++ webapp/src/components/Assembler.vue
@@ -9,30 +9,29 @@
         </div>
         <h2>Input (.asm)</h2>
       </div>
+
+      <div id="editor"></div>
       
-      <textarea 
-      onkeydown="if(event.keyCode===9){var v=this.value,s=this.selectionStart,e=this.selectionEnd;this.value=v.substring(0, s)+'\t'+v.substring(e);this.selectionStart=this.selectionEnd=s+1;return false;}"
-      class="lined" cols="60" rows="39" v-model="asm"></textarea>
-      <h3 v-show="processed" class="text-center">Assemble Errors</h3>
-      <pre v-show="processed">{{errors}}</pre>
-    </div>
-    <div class="col-sm-6">
-      <div class='page-header'>
-        <div class='btn-toolbar pull-right'>
-          <div class='btn-group'>
-            <a v-show="processed" class="btn btn-primary" id="download-asm" download='file.asm' href="data:application/x-asm,">Download Asm</a>
-            <a v-show="processed" class="btn btn-primary" id="download-imem" download='imem.mif' href="data:application/x-mif,">Download Imem</a>
-            <a v-show="processed" class="btn btn-primary" id="download-dmem" download='dmem.mif' href="data:application/x-mif,">Download Dmem</a>
-          </div>
-        </div>
-        <h2>Output (.mif)</h2>
-      </div>
-
-      <pre v-show="processed">{{imem}}</pre>
-      <pre v-show="processed">{{dmem}}</pre>
-
-    </div>
+    <h3 v-show="processed" class="text-center">Assemble Errors</h3>
+    <pre v-show="processed">{{errors}}</pre>
   </div>
+  <div class="col-sm-6">
+    <div class='page-header'>
+      <div class='btn-toolbar pull-right'>
+        <div class='btn-group'>
+          <a v-show="processed" class="btn btn-primary" id="download-asm" download='file.asm' href="data:application/x-asm,">Download Asm</a>
+          <a v-show="processed" class="btn btn-primary" id="download-imem" download='imem.mif' href="data:application/x-mif,">Download Imem</a>
+          <a v-show="processed" class="btn btn-primary" id="download-dmem" download='dmem.mif' href="data:application/x-mif,">Download Dmem</a>
+        </div>
+      </div>
+      <h2>Output (.mif)</h2>
+    </div>
+
+    <pre v-show="processed">{{imem}}</pre>
+    <pre v-show="processed">{{dmem}}</pre>
+
+  </div>
+</div>
 </template>
 
 <script>
@@ -44,33 +43,54 @@
       return {
         processed: false,
         id: '',
-        asm: '',
+        asm: 
+        `.text
+main: addi $1, $0, 1999
+noop
+halt
+sw $r18, -640($r7)
+bne $3, $2, middle
+addi $6, $1, 65535
+addi $7, $1, -65536
+middle:
+sub $8, $2, $1
+and $9, $2, $1
+j quit
+dead: addi $r7, $r0, 0x0000DF00
+quit:
+
+.data
+wow: .word 0x0000B504
+mystring: .string ASDASDASDASDASDASD
+var: .char Z
+label: .char A
+heapsize: .word 0x00000000
+myheap: .word 0x00000000`,
         imem: '',
         dmem: '',
-        errors: ''
+        errors: '',
+        editor: {}
       }
+    },
+
+    ready: function() {
+      this.editor = ace.edit("editor");
+      this.editor.setTheme("ace/theme/github");
+      //this.editor.getSession().setMode("ace/mode/javascript");
+      this.editor.setValue(this.asm);
     },
 
     methods: {
 
       assemble() {
+        //console.log(this.editor.getValue());
         this.$http
-        .post('api/protected/assemble', this.asm, (data) => {
+        .post('api/protected/assemble', this.editor.getValue(), (data) => {
           this.id = data.id;
           this.imem = data.imem;
           this.dmem = data.dmem;
           this.errors = data.errors;
-          var arr = localStorage.getItem("ids");
-          if (arr) {
-            var a = [];
-            a = JSON.parse(arr);
-            a.push(this.id);
-            localStorage.setItem('ids', JSON.stringify(a));
-          } else {
-            var a = [];
-            a.push(this.id);
-            localStorage.setItem('ids', JSON.stringify(a));
-          }
+          
           document.getElementById('download-asm').setAttribute('href', "data:application/x-asm," + encodeURI(this.asm));
           document.getElementById('download-imem').setAttribute('href', "data:application/x-mif," + encodeURI(this.imem));
           document.getElementById('download-dmem').setAttribute('href', "data:application/x-mif," + encodeURI(this.dmem));
@@ -89,6 +109,8 @@
     route: {
       canActivate() {
         //return auth.user.authenticated
+
+        // can always activate because assembler is currently unlocked
         return true;
       }
     }
