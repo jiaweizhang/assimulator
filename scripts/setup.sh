886c529f7d393ce5721cd7199dc9bb7c66a89ec8
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-11-07 00:07:08 -0500

    Removed eslint and updated setup.sh t

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
+#install git
+sudo apt-get install git
+
+#allow add-apt-repository
+sudo apt-get install software-properties-common
+
+#curl
+sudo apt-get install curl
+
 #install java
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
 
+#install sdkman
+sudo apt-get install unzip
+curl -s "https://get.sdkman.io" | bash
+
 #install gradle
-sudo add-apt-repository ppa:cwchien/gradle
-sudo apt-get update
-sudo apt-get install gradle
+sdk install gradle 2.14.1
 
-#install mongodb
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+#install npm
+sudo apt-get install npm
 
-#run mongodb (not necessary unless restart)
-#sudo service mongod start
+#npm no sudo
+git clone https://github.com/glenpike/npm-g_nosudo.git
+cd npm-g_nosudo/
+./npm-g-nosudo.sh
+source ~/.bashrc
 
-#create mongodb database
-#start #start mongo shell
-#use test #create test database
+#fix nodejs -> node
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+#repo
+git clone https://github.com/jiaweizhang/assembler-disassembler-simulator.git
+cd assembler-disassembler-simulator/
+cd src/main/webapp/
+npm install
